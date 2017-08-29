bae41e44c5ad5d89eac4916ce2b52781092a944c
Mina Parnian <minaparnian66@gmail.com>
2017-08-29 11:39:27 +1000

    comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.create comment_params
+    @comment.update :image_id => params[:image_id]
+    @current_user.comments << @comment
+    redirect_to image_path(params[:image_id])
+
+  end
+
+  def update
+    comment = Comment.find params[:id]
+    comment.update comment_params
+    redirect_to new_comment_path
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :image_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
 
 
+
+
   root :to => 'users#new'
   resources :users
   resources :images
+  resources :comments
 
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
-  
+
 
 end
