507035100cdffb3b4f455dae3f2e3cae43569d0e
Mina Parnian <minaparnian66@gmail.com>
2017-08-31 18:02:44 +1000

    project1

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     $('#nav-icon1').click(function(){
         $(this).toggleClass('open');
+        $('.hiddenMenu').toggle()
     });
 });
 
-document.querySelector('.scroll-icon').scrollIntoView({
-  behavior: 'smooth'
-});
+// document.querySelector('.scroll-icon').scrollIntoView({
+//   behavior: 'smooth'
+// });
